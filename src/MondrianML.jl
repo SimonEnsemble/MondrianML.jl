3b35ad5374f585f62a2227e4dd71291367f043e8
Cory Simon <cory.simon@oregonstate.edu>
2026-06-24 21:32:53 -0700

    first

---
+++ src/MondrianML.jl
@@ -0,0 +1,8 @@
+module MondrianML
+
+using Random, Statistics, StatsBase, CairoMakie, ColorSchemes, LinearAlgebra
+
+include("mondrian_partition.jl")
+
+export sample_mondrian_partition
+end # module
